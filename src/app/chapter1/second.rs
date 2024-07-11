0ac274ad70b7534753a9bc51a8960610ee582a7a
Yannick Feld <yannick.feld@uol.de>
2024-07-11 14:05:11 +0200

    Animation Example

--- src/app/chapter1/second.rs
+++ src/app/chapter1/second.rs
@@ -1,12 +1,14 @@
 use derivative::Derivative;
 // This is an example
 use crate::dark_magic::*;
-use egui_plot::{Plot, PlotPoint, PlotPoints, Points};
+use egui_plot::{Plot, PlotPoint, PlotPoints, Points, Line};
+use web_time::Instant;
 
 #[derive(Debug, Derivative)]
 #[derivative(Default)]
 struct SecondData{
     data: Vec<PlotPoint>,
+    start_time: Option<Instant>,
     x: f64,
     y: f64
 }
@@ -14,6 +16,14 @@ struct SecondData{
 pub fn chapter_1_switch(any: &mut BoxedAnything, ctx: &egui::Context)
 {
     let data: &mut SecondData = any.to_something_or_default_mut();
+
+
+    if data.start_time.is_none(){
+        data.start_time = Some(Instant::now());
+    }
+
+    let duration = data.start_time.as_ref().unwrap().elapsed();
+    let seconds = duration.as_secs_f64();
 
     egui::TopBottomPanel::top("my_panel").show(ctx, |ui| {
         ui.add(
@@ -34,10 +44,25 @@ pub fn chapter_1_switch(any: &mut BoxedAnything, ctx: &egui::Context)
 
         let points = PlotPoints::Owned(data.data.clone());
         let points = Points::new(points).radius(4.0);
-        
+
+        let sin: PlotPoints = (0..1000).map(|i| {
+            let x = i as f64 * 0.01 + seconds;
+            [x, x.sin()]
+        }).collect();
+        let line = Line::new(sin);
         Plot::new("my_plot")
-            .view_aspect(2.0)
-            .show(ui, |plot_ui| plot_ui.points(points));
+            .x_axis_label("time")
+            .y_axis_label("Arbitrary stuff")
+            .show(
+                ui, 
+                |plot_ui|
+                {
+                    plot_ui.points(points);
+                    plot_ui.line(line)
+                }
+            );
     });
+
+    ctx.request_repaint();
     
 }
